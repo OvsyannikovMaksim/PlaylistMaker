c4b722c64b4fca01d3fbf65e92eb0da2a2dd9f95
Ovsyannikov_M <maksim.ov@mail.ru>
2024-09-07 17:42:02 +0300

    second

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
 
         val searchButton = findViewById<Button>(R.id.search_button)
         val mediaButton = findViewById<Button>(R.id.media_button)
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -13,5 +16,43 @@ class SettingsActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             finish()
         }
+
+        val shareButton = findViewById<TextView>(R.id.share_button)
+        val supportButton = findViewById<TextView>(R.id.support_button)
+        val eulaButton = findViewById<TextView>(R.id.eula_button)
+
+        shareButton.setOnClickListener {
+            startShareIntent()
+        }
+
+        supportButton.setOnClickListener {
+            startSupportIntent()
+        }
+
+        eulaButton.setOnClickListener {
+            startEulaIntent()
+        }
+    }
+
+    private fun startShareIntent() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer")
+        intent.setType("text/plain")
+        startActivity(Intent.createChooser(intent, "Share Via"))
+    }
+
+    private fun startSupportIntent() {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("maksimov99997@yandex.ru"))
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+        intent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+        startActivity(intent)
+    }
+
+    private fun startEulaIntent()  {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+        startActivity(intent)
     }
 }
