84fa4c787f04163efc48b4c9e9c5a71687c1bc0d
Ovsyannikov_M <maksim.ov@mail.ru>
2025-01-27 19:59:47 +0300

    fixes

--- app/src/main/java/com/example/playlistmaker/search/ui/SearchViewModelFactory.kt
+++ app/src/main/java/com/example/playlistmaker/search/ui/SearchViewModelFactory.kt
@@ -4,7 +4,6 @@ package com.example.playlistmaker.search.ui
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.playlistmaker.search.ui.SearchViewModel
 import com.example.playlistmaker.utils.Creator
 
 object SearchViewModelFactory {
--- app/src/main/java/com/example/playlistmaker/settings/ui/SettingsViewModelFactory.kt
+++ app/src/main/java/com/example/playlistmaker/settings/ui/SettingsViewModelFactory.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.playlistmaker.settings.ui.SettingsViewModel
 import com.example.playlistmaker.utils.Creator
 
 object SettingsViewModelFactory {
