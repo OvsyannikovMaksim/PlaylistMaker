e5c962c1261ebe1108055acac47865d8d12d8bfa
Ovsyannikov_M <maksim.ov@mail.ru>
2025-03-15 01:32:22 +0300

    init navigation

---
+++ app/src/main/java/com/example/playlistmaker/media/ui/fragments/FavTracksFragment.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker.media.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
+
+class FavTracksFragment: Fragment(R.layout.fragment_fav_tracks) {
+
+    companion object{
+        fun newInstance() = FavTracksFragment()
+    }
+}
---
+++ app/src/main/java/com/example/playlistmaker/media/ui/fragments/PlaylistsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker.media.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
+
+class PlaylistsFragment: Fragment(R.layout.fragment_playlists) {
+
+    companion object{
+        fun newInstance() = PlaylistsFragment()
+    }
+}
